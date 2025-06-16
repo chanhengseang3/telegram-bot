a7abea65dbf3e57b8bc448e9babfa62e5f1ac309
chanhengseang <chanheng.seang@mbanq.com>
2025-06-15 20:01:46 -0700

    env

--- src/main/java/asia/igsaas/Bot.java
+++ src/main/java/asia/igsaas/Bot.java
@@ -6,7 +6,6 @@ import asia.igsaas.helper.CommandHandler;
 import asia.igsaas.helper.ReplyHandler;
 import asia.igsaas.service.IncomeService;
 import asia.igsaas.utils.PaywayParser;
-import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -23,16 +22,11 @@ import java.math.BigDecimal;
 @Component
 public class Bot extends TelegramLongPollingBot {
 
-    //    private static final String BOT_TOKEN = "7188303916:AAEbEbMBabwoyuY-G7rcRIZd-63E2zZywDc";
-    private static final String BOT_TOKEN = "7681368869:AAE5QlSXTOkeQMuJ3S6znuyCeyPVvo1YWFs";
-
     private final IncomeService incomeService;
     private final ButtonCallbackHandler callbackHandler;
     private final CommandHandler commandHandler;
     private final ReplyHandler replyHandler;
-
-    @Value("${bot.name}")
-    private String botName;
+    private final String botName;
 
     @Override
     public String getBotUsername() {
@@ -40,6 +34,7 @@ public class Bot extends TelegramLongPollingBot {
     }
 
     public Bot(@Value("${bot.token}") String botToken,
+               @Value("${bot.name}") String botName,
                IncomeService incomeService,
                ButtonCallbackHandler callbackHandler,
                CommandHandler commandHandler,
@@ -49,11 +44,12 @@ public class Bot extends TelegramLongPollingBot {
         this.callbackHandler = callbackHandler;
         this.commandHandler = commandHandler;
         this.replyHandler = replyHandler;
+        this.botName = botName;
         TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
         botsApi.registerBot(this);
     }
 
-    @PostConstruct
+//    @PostConstruct
     private void init() {
         String text = """
                 ៛114,800 paid by Sao Dy (*210) on Jun 15, 11:49 PM via ABA KHQR (ACLEDA Bank Plc.) at MISS 16 by T.KIM. Trx. ID: 175000618948483, APV: 603982.
